ed4360bc25810608eb206cc3dbfdf0db4410e329
Bastien Mothais <bastien.mothais@gmail.com>
2015-02-27 09:53:53 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE HTML>
+<!--
+	Landed by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Bastien Mothais</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.scrolly.min.js"></script>
+		<script src="js/jquery.dropotron.min.js"></script>
+		<script src="js/jquery.scrollex.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<script type="text/javascript">
+			function afficher() {
+				alert("You have been subscribed properly!")
+			}
+		</script>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-60123254-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
+	</head>
+	<body class="landing">
+
+		<!-- Header -->
+			<header id="header">
+				<h1 id="logo"><a href="http://www.bastienmothais.com/">Bastien Mothais</a></h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="http://www.bastienmothais.com/">Presentation</a></li>
+						<li><a href="#five" class="button special scrolly">Sign Up</a></li>
+					</ul>
+				</nav>
+			</header>
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="content">
+					<header>
+						<h2>Hello, I'm Project Manager</h2>
+						<p>Planning without action is futile,<br />
+						action without planning is fatal. <br />
+						<span>- Cornelius</span></p>
+					</header>
+					<span class="image"><img src="images/profile.jpg" alt="" /></span>
+				</div>
+				<a href="#five" class="goto-next scrolly">Next</a>
+			</section>
+
+
+		<!-- Five -->
+			<section id="five" class="wrapper style2 special fade">
+				<div class="container">
+					<header>
+						<h2>Website under construction</h2>
+						<p>You can easily sign up to the newsletter below to be notified about the website opening.</p>
+					</header>
+					<form name="insertion" action="script.php" method="POST" class="container 50%">
+						<div class="row uniform 50%">
+							<div class="8u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Your Email Address" /></div>
+							<div class="4u$ 12u$(xsmall)"><input type="submit" value="Sign up" class="fit special" onclick="afficher()" /></div>
+						</div>
+					</form>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<ul class="icons">
+					<li><a href="https://es.linkedin.com/pub/bastien-mothais/54/314/22" target="_blank" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
+					<li><a href="https://www.facebook.com/bastien.mothais" target="_blank" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+					<li><a href="https://twitter.com/m_bastien" target="_blank" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="mailto:bastien.mothais@gmail.com" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+				</ul>
+				<ul class="copyright">
+					<li>2015 - Bastien Mothais</li><li>Design: <a href="http://html5up.net" target="_blank">HTML5 UP</a></li>
+				</ul>
+			</footer>
+
+	</body>
+</html>
